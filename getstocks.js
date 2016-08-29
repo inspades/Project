1567164d318dbe41f657884588e58d12defc1d0a
inspades <cale.gray.slc@gmail.com>
2016-08-29 03:59:23 -0600

    Update getstocks.js

--- getstocks.js
+++ getstocks.js
@@ -1,14 +1,14 @@
-function getData() {
-    var url = "http://query.yahooapis.com/v1/public/yql";
-    var symbol = $("#symbol").val();
-    var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
+ function getPrice() {
+     var url = "http://query.yahooapis.com/v1/public/yql";
+     var spx = "^GSPC";
+     var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + spx + "')");
 
-    $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
-        .done(function (data) {
-        $("#result").text("Bid Price: " + data.query.results.quote.LastTradePriceOnly);
-    })
-        .fail(function (jqxhr, textStatus, error) {
-        var err = textStatus + ", " + error;
-            $("#result").text('Request failed: ' + err);
-    });
-}
+     $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
+         .done(function (data) {
+         $("#currentprice").text( data.query.results.quote.LastTradePriceOnly);
+     })
+         .fail(function (jqxhr, textStatus, error) {
+         var err = textStatus + ", " + error;
+             $("#currentprice").text('Request failed: ' + err);
+     });
+ }
